516c05b404070677afdffeb56b712d62afd1d503
Leandro <atiladevelop@gmail.com>
2022-01-08 11:47:01 -0300

    avatar changed to jpg format

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Avatar } from '@chakra-ui/react';
 
 const Logo: React.FC = () => {
-  return <Avatar size='md' name='Leandro Favre' src='/avatar.png' />;
+  return <Avatar size='md' name='Leandro Favre' src='/avatar.jpg' />;
 };
 
 export default Logo;
