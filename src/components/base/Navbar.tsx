9be5c977da35dba8ccc671f5ff515385f0acdb2d
Leandro Favre <atiladevelop@gmail.com>
2024-11-04 03:36:20 -0300

    looking issue

--- src/components/base/Navbar.tsx
+++ src/components/base/Navbar.tsx
@@ -5,9 +5,6 @@ export default function Navbar() {
     <nav className="mx-auto max-w-4xl bg-gray-800 p-4 text-orange-100">
       <div className="mx-auto flex items-center justify-between">
         <div className="flex items-center space-x-2 font-ubuntu">
-          <Avatar>
-            <AvatarImage src="https://github.com/atiladev.png" />
-          </Avatar>
           <span className="cursor-pointer text-xl font-medium hover:text-primary">
             Atila<span className="text-primary">Dev</span>
           </span>
